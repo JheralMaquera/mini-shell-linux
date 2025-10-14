a87c29f51cc2bce463b140e15176fe9fd69f16f1
JheralMaquera <jjmaqueral@unjbg.edu.pe>
2025-10-14 16:25:09 -0500

    Mejora del prompt

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,22 @@ using namespace std;
 
 int main(){
     string comando;
-
+    string celeste = "\033[36m"; 
+    string reset = "\033[0m";
+    system("clear");
+    string spaces = "\t\t";
+    cout<<celeste;
+    cout << spaces<<" ▄▄▄  ▄▄▄     ██                  ██               ▄▄                  ▄▄▄▄      ▄▄▄▄     \n";
+    cout << spaces<<" ███  ███     ▀▀                  ▀▀               ██                  ▀▀██      ▀▀██     \n";
+    cout << spaces<<" ████████   ████     ██▄████▄   ████     ▄▄█████▄  ██▄████▄   ▄████▄     ██        ██     \n";
+    cout << spaces<<" ██ ██ ██     ██     ██▀   ██     ██     ██▄▄▄▄ ▀  ██▀   ██  ██▄▄▄▄██    ██        ██     \n";
+    cout << spaces<<" ██ ▀▀ ██     ██     ██    ██     ██      ▀▀▀▀██▄  ██    ██  ██▀▀▀▀▀▀    ██        ██     \n";
+    cout << spaces<<" ██    ██  ▄▄▄██▄▄▄  ██    ██  ▄▄▄██▄▄▄  █▄▄▄▄▄██  ██    ██  ▀██▄▄▄▄█    ██▄▄▄     ██▄▄▄  \n";
+    cout << spaces<<" ▀▀    ▀▀  ▀▀▀▀▀▀▀▀  ▀▀    ▀▀  ▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀   ▀▀    ▀▀    ▀▀▀▀▀      ▀▀▀▀      ▀▀▀▀  \n";
+    cout << spaces<<"----------------------------------Bienvenido a minishell---------------------------------\n"; 
+    cout << endl;
+    cout << endl;
+    cout<<reset;
     while(true){
         cout<<"minishell> ";
         if (!getline(cin, comando)) {
